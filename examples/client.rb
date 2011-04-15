6c88c8281186ebb207b491d3d2fda99cfe0cd9f3
Dylan Egan <me@dylanegan.com>
2011-04-14 22:20:59 -0700

    Client example.

---
+++ examples/client.rb
@@ -0,0 +1,15 @@
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require 'rubygems'
+require 'fraggle/block'
+
+client = Fraggle::Block.connect
+rev = client.set('/foo', 'test', 0).rev
+puts "Setting /foo to test with rev #{rev}"
+foo = client.get('/foo')
+puts "Got /foo with #{foo.value}"
+root = client.getdir('/')
+puts "Directly under / is #{root.collect { |file| file.path }.join(', ')}"
+client.del('/foo', rev)
+puts "Deleted /foo"
+foo = client.get('/foo')
+puts foo.inspect
